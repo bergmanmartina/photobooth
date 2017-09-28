b54efad32184276247e72a863f98be3b00888d72
bergmanmartina <martinabergman91@gmail.com>
2017-09-28 11:31:34 +0200

    ny

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,1 +1,17 @@
-const video = document.getElementById('camera-stream')
+const video = () => {
+	console.log ('camera-stream')
+}
+
+navigator.getMedia = (navigator.getUserMedia ||
+          navigator.webkitGetUserMedia ||
+          navigator.mozGetUserMedia ||
+          navigator.msGetUserMedia);
+
+navigator.getMedia( { video: true }, (stream) => {
+  video.src = window.URL.createObjectURL(stream);
+  video.play();
+  },
+  (err) => {
+    console.error(err);
+  }
+);
